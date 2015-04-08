7b0c5de87d012553840a8dbd479cf72b0df2d06f
suaxu <43046435@qq.com>
2015-04-08 21:41:20 +0800

    pathinfo

--- class/controller/api/sport/pathInfo.php
+++ class/controller/api/sport/pathInfo.php
@@ -8,7 +8,6 @@ class PathInfo extends \Controller\Api
         $pathInfo = array('01-1'=>array('status'=>1,'message'=>'通关','passTime'=>date('Y-m-d H:i:s')),
             '01-2'=>array('status'=>2,'message'=>'未知','passTime'=>date('Y-m-d H:i:s')),
             '01-3'=>array('status'=>3,'message'=>'正常通关','passTime'=>date('Y-m-d H:i:s')),
-
         );
         echo   json_encode($pathInfo);
         //运动员自己的
