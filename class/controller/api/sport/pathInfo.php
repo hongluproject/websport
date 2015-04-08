3a1b9cd1c6256c1e7241efde84a5a7bb64ce3166
suaxu <43046435@qq.com>
2015-04-08 23:43:01 +0800

    pathinfo

--- class/controller/api/sport/pathInfo.php
+++ class/controller/api/sport/pathInfo.php
@@ -6,9 +6,10 @@ class Pathinfo extends \Controller\Api
     public function get()
     {
 
-        $pathInfo = array('1'=>array('status'=>1,'message'=>'通关','passTime'=>date('Y-m-d H:i:s'),'address'=>'下关在哪里'),
-            '2'=>array('status'=>2,'message'=>'未知','passTime'=>date('Y-m-d H:i:s'),'address'=>'下关在哪里1'),
-            '3'=>array('status'=>3,'message'=>'正常通关','passTime'=>date('Y-m-d H:i:s'),'address'=>'下关在哪里12'),
+        $pathInfo = array('status'=>1,'message'=>'OK','result'=>array(array('status'=>1,'message'=>'通关','passTime'=>date('Y-m-d H:i:s'),'address'=>'下关在哪里'),
+                    array('status'=>2,'message'=>'未知','passTime'=>date('Y-m-d H:i:s'),'address'=>'下关在哪里1'),
+                    array('status'=>3,'message'=>'正常通关','passTime'=>date('Y-m-d H:i:s'),'address'=>'下关在哪里12'),
+                    array('status'=>3,'message'=>'正常通关','passTime'=>null,'address'=>'')),
         );
         echo   json_encode($pathInfo);
         //运动员自己的
