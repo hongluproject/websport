c6dd47f94719ef2e9a3254cb0f054aec221a55a6
suaxu <43046435@qq.com>
2015-04-09 15:14:04 +0800

    pathinfo.php

---
+++ class/controller/api/sport/pathinfo.php
@@ -0,0 +1,24 @@
+<?php
+namespace Controller\Api\Sport;
+
+class Pathinfo extends \Controller\Api
+{
+    public function get()
+    {
+
+        $pathInfo = array('status'=>1,'message'=>'01','result'=>array(array('status'=>1,'message'=>'通关','passTime'=>date('Y-m-d H:i:s'),'address'=>'下关在哪里'),
+                    array('status'=>2,'message'=>'02','passTime'=>date('Y-m-d H:i:s'),'address'=>'下关在哪里1'),
+                    array('status'=>3,'message'=>'03','passTime'=>date('Y-m-d H:i:s'),'address'=>'下关在哪里12'),
+                    array('status'=>3,'message'=>'04','passTime'=>null,'address'=>'')),
+        );
+        echo   json_encode($pathInfo);
+        //运动员自己的
+
+    }
+
+    public function post()
+    {
+
+    }
+}
+
