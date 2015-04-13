b8b0124b49ffbb39fc1bd9f5268ae6c33ffd2503
suaxu <43046435@qq.com>
2015-04-13 11:52:33 +0800

    memeberstatus

--- class/controller/api/sport/pathinfo.php
+++ class/controller/api/sport/pathinfo.php
@@ -7,14 +7,12 @@ class Pathinfo extends \Controller\Api
     {
 
         $pathInfo = array('status'=>1,'message'=>'OK','passurl'=>'http://www.baidu.com',
-                    'result'=>array(array('status'=>1,'message'=>'通关','passTime'=>date('Y-m-d H:i:s'),'address'=>'下关在哪里'),
-                    array('status'=>2,'message'=>'02','passTime'=>date('Y-m-d H:i:s'),'address'=>'下关在哪里1'),
-                    array('status'=>3,'message'=>'03','passTime'=>date('Y-m-d H:i:s'),'address'=>'下关在哪里12'),
-                    array('status'=>3,'message'=>'04','passTime'=>null,'address'=>'')),
+                    'result'=>array(array('memberStatus'=>1,'message'=>'通关','passTime'=>date('Y-m-d H:i:s'),'address'=>'下关在哪里'),
+                    array('memberStatus'=>2,'message'=>'02','passTime'=>date('Y-m-d H:i:s'),'address'=>'下关在哪里1'),
+                    array('memberStatus'=>3,'message'=>'03','passTime'=>date('Y-m-d H:i:s'),'address'=>'下关在哪里12'),
+                    array('memberStatus'=>3,'message'=>'04','passTime'=>null,'address'=>'')),
         );
         echo   json_encode($pathInfo);
-        //运动员自己的
-
     }
 
     public function post()
