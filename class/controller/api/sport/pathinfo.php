def0b08f2c516c4c81bf63a9ac5f00c5c7910629
suaxu <43046435@qq.com>
2015-04-13 20:34:51 +0800

    任务

--- class/controller/api/sport/pathinfo.php
+++ class/controller/api/sport/pathinfo.php
@@ -5,21 +5,14 @@ class Pathinfo extends \Controller\Api
 {
     public function get()
     {
-
-
         $phone = $_GET['phone'];
-
-
         $siteModel = new \Model\Site();
         $where = array('lineId'=>'03');
         $sites = $siteModel->fetch($where, 1000);
-
         $pathInfo = array();
-
         foreach($sites as $item){
             $pathInfo[$item->siteId] =  array('memberStatus'=>'3','passTime'=>null,'address'=>$item->position);
         }
-
         echo json_encode(array('status'=>1,'message'=>'OK','passurl'=>'http://www.baidu.com','result'=>$pathInfo));
     }
 
--- class/controller/api/sport/scan.php
+++ class/controller/api/sport/scan.php
@@ -5,6 +5,9 @@ class Scan extends \Controller\Api
 {
     public function get()
     {
+
+
+        $siteId= $_GET['siteID'];
         $type = $_GET['type'];
         if(!$type){
             echo  json_encode(array('status'=>2,'message'=>'没有type'));
@@ -12,13 +15,20 @@ class Scan extends \Controller\Api
         }else if(!$_GET['phone']){
             echo  json_encode(array('status'=>2,'message'=>'没有注册号'));
         }else{
-              if($type== 1){
-                  // todo input databaase;
-                  echo json_encode(array('status'=>1,'message'=>'下面是任务','result'=>array('type'=>$type,'title'=>'任务描述。这个你这次跳楼一百次','url'=>'http://www.baidu.com')));
-              }elseif($type == 2){
-                  //todo input databases;
-                  echo  json_encode(array('status'=>1,'message'=>'顺利通关','result'=>array('type'=>$type,'isFinal'=>false)));
-              }
+              if($type== 2){
+
+                  if($siteId ==0){
+                      echo  json_encode(array('status'=>1,'message'=>'顺利通关','result'=>array('type'=>$type,'isFinal'=>false)));
+                  }elseif($siteId ==1){
+                     echo  json_encode(array('status'=>1,'message'=>'顺利通关','result'=>array('type'=>$type,'isFinal'=>false)));
+                  }elseif($siteId ==2){
+                      echo  json_encode(array('status'=>1,'message'=>'顺利通关','result'=>array('type'=>$type,'isFinal'=>false)));
+                  }elseif($siteId == 8){
+                      echo  json_encode(array('status'=>1,'message'=>'顺利通关','result'=>array('type'=>$type,'isFinal'=>true,'passurl'=>'http://www.baidu.com')));
+                  }
+              }elseif($type == 1){
+                   echo json_encode(array('status'=>1,'message'=>'下面是任务','result'=>array('type'=>$type,'title'=>'任务描述。这个你这次跳楼一百次','url'=>'http://www.baidu.com')));
+               }
         }
     }
 
