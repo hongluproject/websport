a4e473506b51c5b89b55e49668d08257cc7e4e23
suaxu <43046435@qq.com>
2015-04-14 18:01:11 +0800

    scan 提示

--- class/controller/api/sport/scan.php
+++ class/controller/api/sport/scan.php
@@ -5,61 +5,85 @@ class Scan extends \Controller\Api
 {
     public function get()
     {
+        //站点
         $siteId = $_GET['siteId'];
+        //线路
         $lineId = $_GET['lineId'];
         $phone = $_GET['phone'];
+        //所处段
+        $position = $_GET['position'];
+        //1任务信息 2是 过关信息
         $type = $_GET['type'];
         if (!$type) {
             echo json_encode(array('status' => 2, 'message' => '没有type'));
-            exit;
         } else if (!$phone) {
-            echo json_encode(array('status' => 2, 'message' => '没有注册号'));
+            echo json_encode(array('status' => 2, 'message' => '没有录入手机号码'));
+        } else if (!isset($siteId)) {
+            echo json_encode(array('status' => 2, 'message' => '没有站点信息'));
+        } else if (!$lineId) {
+            echo json_encode(array('status' => 2, 'message' => '没有线路信息'));
         } else {
             $memberModel = new \Model\Member();
             $where = array('phone' => $phone);
-            $member = $memberModel->fetch($where, 1);
+            $member = $memberModel->find($where);
             if ($member) {
-                $teamId = $member[0]->teamId;
+                //找到member
+                $teamId = $member->teamId;
                 if ($teamId) {
                     $teamModel = new \Model\Team();
                     $where = array('teamId' => $teamId);
-                    $team = $teamModel->fetch($where, 1);
+                    $team = $teamModel->find($where);
                     //找到team
                     if ($team) {
                         if ($type == 2) {
-                            $passInfo =(array)json_decode($team[0]->pathInfo,true);
-                            if (array_key_exists($lineId.'-'.$siteId, $passInfo)) {
-
-                                if ($siteId != 8) {
-                                    echo json_encode(array('status' => 1, 'message' => '已经扫描过关卡', 'result' => array('type' => $type, 'isFinal' => false)));
-                                } else {
+                            $passInfo = (array)json_decode($team->pathInfo, true);
+                            if (array_key_exists($lineId . '-' . $siteId, $passInfo)) {
+                                if ($position == 3) {
                                     echo json_encode(array('status' => 1, 'message' => '已经扫描过关卡', 'result' => array('type' => $type, 'isFinal' => true, 'passurl' => 'http://www.baidu.com')));
+                                } else {
+                                    echo json_encode(array('status' => 1, 'message' => '已经扫描过关卡', 'result' => array('type' => $type, 'isFinal' => false)));
                                 }
                             } else {
-                                $passInfo[$lineId.'-'.$siteId] = array('memberStatus' => 1, 'passTime' => date('Y-m-d H:i:s'));
-                                $teamInfo = \Model\Team::find($team[0]->id);
-                                $param['pathInfo']= json_encode($passInfo);
+                                $passInfo[$lineId . '-' . $siteId] = array('memberStatus' => 1, 'passTime' => date('Y-m-d H:i:s'));
+                                $teamInfo = \Model\Team::find($team->id);
+                                $param['pathInfo'] = json_encode($passInfo);
                                 $teamInfo->set($param);
                                 $teamInfo->save();
-                                if ($siteId != 8) {
-                                    echo json_encode(array('status' => 1, 'message' => '顺利通关', 'result' => array('type' => $type, 'isFinal' => false)));
-                                } else {
+                                if ($position == 3) {
                                     echo json_encode(array('status' => 1, 'message' => '顺利通关', 'result' => array('type' => $type, 'isFinal' => true, 'passurl' => 'http://www.baidu.com')));
+                                } else {
+                                    echo json_encode(array('status' => 1, 'message' => '顺利通关', 'result' => array('type' => $type, 'isFinal' => false)));
                                 }
-                             }
+                            }
                         } elseif ($type == 1) {
-                            //todo insert db
-                            echo json_encode(array('status' => 1, 'message' => '下面是任务', 'result' => array('type' => $type, 'title' => '任务描述。这个你这次跳楼一百次', 'url' => 'http://www.baidu.com')));
+                            $missionInfo = (array)json_decode($team->missionInfo, true);
+                            if (array_key_exists($lineId . '-' . $siteId, $missionInfo)) {
+                                $missionInfoTitle = $missionInfo[$lineId . '-' . $siteId]['title'];
+                                $missionInfoUrl   = $missionInfo[$lineId . '-' . $siteId]['url'];
+                                echo json_encode(array('status' => 1, 'message' => '已领取任务', 'result' => array('type' => $type, 'title' => $missionInfoTitle, 'url' => $missionInfoUrl)));
+                            }else{
+                                //随机任务
+                                $siteModel = new \Model\Site();
+                                $where = array('siteId' => $siteId,'lineId'=>$lineId);
+                                $site = $siteModel->find($where);
+                                $siteMission = json_decode($site->mission,true);
+                                $rendSiteMission = array_rand($siteMission);
+                                //录入个人任务
+                                $missionInfo[$lineId . '-' . $siteId] = array('title' => $rendSiteMission, 'url' => $siteMission[$rendSiteMission]);
+                                $teamInfo = \Model\Team::find($team->id);
+                                $param['missionInfo'] = json_encode($missionInfo);
+                                $teamInfo->set($param);
+                                $teamInfo->save();
+                                echo json_encode(array('status' => 1, 'message' => '下面是任务', 'result' => array('type' => $type, 'title' => $rendSiteMission, 'url' => $siteMission[$rendSiteMission])));
+                            }
                         }
-
                     } else {
-                        echo 'no team find';
+                        echo json_encode(array('status' => 2, 'message' => '没有找到队伍'));
                     }
                 }
             } else {
-                echo "no member find";
+                echo json_encode(array('status' => 2, 'message' => '没有成员'));
             }
-
         }
     }
 
