0ad2648a3ced1f624351f71f2c71a44bc26be43a
suaxu <43046435@qq.com>
2015-04-15 18:04:50 +0800

    list

--- view/admin/site/list.php
+++ view/admin/site/list.php
@@ -10,8 +10,8 @@
             <tr>
                 <th width="20">Id</th>
                 <th width="100">点标号</th>
-                <th width="100">二维码链接</th>
-                <th width="100">任务二维码链接</th>
+              <!--  <th width="100">二维码链接</th>
+                <th width="100">任务二维码链接</th>-->
                 <th width="100">点标名称</th>
                 <th width="100">点标位置</th>
                 <th width="100">分配到线路</th>
@@ -28,16 +28,17 @@
 
                         <td><?php echo $item->id; ?></td>
                         <td><?php echo $item->siteId; ?></td>
+                        <!--
                         <td>
-                            <?php echo "http://sport.hoopeng.cn/api/sport/scan?type=2&lineId=".$item->lineId."&siteId=".$item->siteId.htmlspecialchars("&section=").$item->section."&md=".md5($item->lineId.'-'.$item->siteId);?>
+                            <?php /*echo "http://sport.hoopeng.cn/api/sport/scan?type=2&lineId=".$item->lineId."&siteId=".$item->siteId.htmlspecialchars("&section=").$item->section."&md=".md5($item->lineId.'-'.$item->siteId);*/?>
                         </td>
 
 
                         <td>
-                            <?php if ($item->passInfo == 2):?>
-                            <?php echo "http://sport.hoopeng.cn/api/sport/scan?type=1&lineId=".$item->lineId."&siteId=".$item->siteId.htmlspecialchars("&section=").$item->section;?>
-                            <?php endif ;?>
-                        </td>
+                            <?php /*if ($item->passInfo == 2):*/?>
+                            <?php /*echo "http://sport.hoopeng.cn/api/sport/scan?type=1&lineId=".$item->lineId."&siteId=".$item->siteId.htmlspecialchars("&section=").$item->section;*/?>
+                            <?php /*endif ;*/?>
+                        </td>-->
 
 
                         <td><?php echo $item->siteName; ?></td>
@@ -50,16 +51,16 @@
                         <td><?php echo $item->siteManager; ?></td>
                         <td>
                             <a href="/admin/site/edit/<?php echo $item->id; ?>" target="_blank"><i class="icon-ok" alt="更新" title="更新"></i></a>&nbsp;&nbsp;&nbsp;
-                            <a href="/admin/site/delete/<?php echo $item->id; ?>" class="delete"><i class="icon-remove" alt="删除" title="删除"></i></a>&nbsp;&nbsp;&nbsp;
-                        </td>
+<!--                            <a href="/admin/site/delete/<?php /*echo $item->id; */?>" class="delete"><i class="icon-remove" alt="删除" title="删除"></i></a>&nbsp;&nbsp;&nbsp;
+-->                        </td>
                     </tr>
 
                 <?php endforeach; ?>
             <tr>
                 <th width="20">Id</th>
                 <th width="100">点标号</th>
-                <th width="100">二维码链接</th>
-                <th width="100">任务二维码链接</th>
+        <!--        <th width="100">二维码链接</th>
+                <th width="100">任务二维码链接</th>-->
                 <th width="100">点标名称</th>
                 <th width="100">点标位置</th>
                 <th width="100">分配到线路</th>
