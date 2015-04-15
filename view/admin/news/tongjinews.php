d2606f88591b96b0237f77e4625aed90745d2e04
suaxu <43046435@qq.com>
2015-04-15 21:32:15 +0800

    删除无用数据

--- view/admin/news/tongjinews.php
+++
@@ -1,8 +0,0 @@
-<div class="well sidebar-nav">
-    <ul class="nav nav-list">
-      <?php foreach($result->results as $key=> $item):?>
-     <li> <?php echo $key+1;?>:  <?php echo $item->title?><span style="float: right"><a href="http://admin.hoopeng.cn/admin/news/update/<?php echo $item->objectId?>" target="_blank">编辑</a> &nbsp;&nbsp;<a href="https://hoopeng.avosapps.com/news/<?php echo $item->objectId?>" target="_blank">预览</a><span></li>
-     <?php endforeach;?>
-
-    </ul>
-</div>
