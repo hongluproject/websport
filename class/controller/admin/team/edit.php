9c6b38f04459cc21a6b5887c5439edb3a987ae97
suaxu <43046435@qq.com>
2015-06-05 22:02:14 +0800

    teamdit

--- class/controller/admin/team/edit.php
+++ class/controller/admin/team/edit.php
@@ -13,6 +13,53 @@ class Edit extends \Controller\Admin\Team
 
 
         $db = \Model\Line::db();
+       /* $member = $db->fetch('select * from  `ma_member` ');
+        $temp = array();
+        $temp1 = array();
+
+        foreach($member as $item){
+            if(in_array($item->teamId,$temp[$item->phone]))
+                continue;
+            $temp[$item->phone][] =$item->teamId;
+        }
+
+
+        foreach($temp as $key=>$item){
+            if(count($item)>1){
+                $temp1[$key]=$item;
+            }
+        }
+
+
+
+
+
+        $abc  =array();
+        foreach($temp1 as $key=> $item){
+            if(count($item)>1){
+                $abc[$key] =$item;
+
+                echo "手机号：".$key."&nbsp;&nbsp;(". implode(',',$item).")<br>";
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        $db = \Model\Line::db();*/
         $table = 'ma_line';
         $this->lineList= $db->fetch('select * from  `ma_line` ');
         if ($id) {
